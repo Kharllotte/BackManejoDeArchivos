22f5475ceed1e933a7080eed4266952e96cf966d
Liliana <kharlotte@protonmail.com>
2023-06-16 00:19:16 -0500

    update project

--- ProductManager.js
+++ ProductManager.js
@@ -1,99 +1,99 @@
 const fs = require("fs");
 
-const productos = []
-
-class ProductManager {
-
-    static countId = 0
-
-    constructor(title, description, price, thumbnail, code, stock){
-        this.path = "./files",  //Ruta donde se guardaran los archivos
-        this.title = title,
-        this.description = description,
-        this.price = price,
-        this.thumbnail = thumbnail,
-        this.code = code,
-        this.stock = stock   
+class ProductManager{
+    static id = 0
+    constructor(){
+        this.path = "./products.json";
+        this.products = []
     }
 
-    contadorId(){
-        let id = productos.length
-        id = id+1
-        return id
-    }
-    
-    addProduct(){
-        console.log(this.title)
-        let id = this.contadorId()
-        let title = this.title
-        let description = this.description
-        let price = this.price
-        let thumbnail = this.thumbnail
-        let code = this.code
-        let stock = this.stock
-        const product = {id, title, description, price, thumbnail, code, stock}
-        console.log(product)
-        productos.push(product)
-        console.log(productos)
-        fs.promises.writeFile(`${this.path}/Products.json`, JSON.stringify(productos))
-
+    addProduct = async (title, description, price, thumbnail, code, stock) => {
+        ProductManager.id++
+        let newProduct = {id: ProductManager.id, title, description, price, thumbnail, code, stock};
+        this.products.push(newProduct);
+        fs.promises.writeFile(this.path, JSON.stringify(this.products), "utf-8")
+        .then(()=> console.log("Producto añadido exitosamente"))
+        .catch((error) => console.error("Error No se pudo agregar el producto:", error));
+        
     }
 
-    getProducts= async ()=>{
-        if (fs.existsSync(this.path)) {
-            const data = await fs.promises.readFile(this.path, 'utf-8')
-            console.log(data)
-            const arrayJava = JSON.parse(data)
-            console.log(arrayJava)
-            return arrayJava
-        } else {
-            return console.log([])
-        } 
+    getProducts = async () => {
+        try {
+          const data = await fs.promises.readFile(this.path, "utf-8");
+          const products = JSON.parse(data);
+          console.log(products);
+        } catch (error) {
+          console.error("No se pudo obtener los productos", error);
+        }
     }
 
-    getProductByid= async (id) =>{
-        let arrayJava = await this.getProducts()
-        let arrayId =arrayJava.find(producto => producto.id === id)
-        console.log(arrayId)
+    getProductByID = async (id) => {
+      try {
+        const data = await fs.promises.readFile(this.path, "utf-8");
+        const products = JSON.parse(data);
+        const findProduct = products.find((x) => x.id === id);
+        console.log(findProduct);
+      } catch (error) {
+        console.error("Error No se pudo obtener el producto por id:", error);
+      }
     }
 
-    updateProducts = async ({id, ...producto}) =>{
-        await this.deleteProducts(id)
-        let producroViejo = await this.getProducts();
-        let editado =[{id, ...producto}, ...producroViejo]
+    deleteProduct = async (id) => {
+      try {
+        const data = await fs.promises.readFile(this.path, "utf-8");
+        if (data.trim().length === 0) {
+          console.log("El archivo está vacío");
+          return;
+        }
+        const products = JSON.parse(data);
+        const updateProduct = products.filter((x) => x.id !== id);
+        if (products.length === updateProduct.length) {
+          console.log("No se encontró ningún producto con el id");
+          return;
+        }
+        await fs.promises.writeFile(this.path, JSON.stringify(updateProduct), "utf-8");
+        console.log("El producto se ha eliminado exitosamente");
+      } catch (error) {
+        console.error("Error No se pudo eliminar el producto:", error);
+      }
     }
 
-    deleteProducts = async(id) =>{
-        let arrayJava = await this.getProducts()
-        let arrayFilter = arrayJava.filter(producto => producto.id != id)
-        console.log(arrayFilter);
-        console.log("producto eliminado");
-    }
-
+    updateProducts = async (id, newData) => {
+      try {
+        const data = await fs.promises.readFile(this.path, "utf-8");
+        if (data.trim().length === 0) {
+          console.log("El archivo está vacío.");
+          return;
+        }
+        const products = JSON.parse(data);
+        const productById = products.findIndex((x) => x.id === id);
+        if (productById === -1) {
+          console.log("No se encontró ningún producto con el id");
+          return;
+        }
+        products[productById] = { ...products[productById], ...newData };
+        await fs.promises.writeFile(this.path, JSON.stringify(products), "utf-8");
+        console.log("El producto se actualizo exitosamente.");
+      } catch (error) {
+        console.error("Error no se pudo actualizar el producto:", error);
+      }
+    }      
+      
 }
 
-const producto1 = new ProductManager("producto1", "DescripcionProducto1", 30, "Sin imagen1", "abc123", 25)
-const producto2 = new ProductManager("producto2", "DescripcionProducto2", 25, "Sin imagen2", "def456", 2)
-const producto3 = new ProductManager("producto3", "DescripcionProducto3", 76, "Sin imagen3", "ghi789", 7)
 
+const producto1 = {title: "producto1", description: "DescripcionProducto1", price: 30, thumbnail: "SinImagen1", code: "abc123", stock: 25}
+const producto2 = {title: "producto2", description: "DescripcionProducto2", price: 25, thumbnail: "SinImagen2", code: "def456", stock: 2}
+const producto3 = {title: "producto3", description: "DescripcionProducto3", price: 76, thumbnail: "SinImagen3", code: "ghi789", stock: 7}
+const updatedData = {title: "producto 24", description: "fewfv", price: 25, thumbnail: "veew", code: "ewgfwe", stock: 100};
 
-producto1.addProduct()
-console.log("Ver segundo producto")
-producto2.addProduct()
-producto3.addProduct()
-console.log("GET");
-producto3.getProducts()
-console.log("Delete");
-producto1.deleteProducts(1)
-producto3.updateProducts(
-    {
-        id: 3,
-        title: 'Actualizando producto',
-        description: 'Producto actualizad0',
-        price: 200,
-        thumbnail: 'Sin imagen3',
-        code: 'ghifd789',
-        stock: 5
-      }
-)
-// console.log(producto1.contadorId())
+const productos = new ProductManager();
+productos.addProduct(producto1);
+productos.addProduct(producto2);
+productos.addProduct(producto3);
+
+// productos.getProducts();
+// productos.getProductByID(2);
+// productos.deleteProduct(1);
+// productos.updateProducts(2, updatedData);
+
